dd812f9e366c4b7cf8092f6e81da3c760a5c7f54
202202571 <whdgnsdl0812@gmail.com>
2025-07-29 20:39:59 +0900

    Title: 폭탄 해제, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250729/폭탄 해제/bomb-removal.py
@@ -0,0 +1,15 @@
+class Bomb:
+    def __init__(self,code,color,candle):
+        self.code = code
+        self.color = color
+        self.candle =candle
+    
+    def __str__(self):
+        return f"code : {self.code}\ncolor : {self.color}\nsecond : {self.candle}"
+
+unlock_code,color,candle = input().split()
+
+unlock_bomb = Bomb(unlock_code,color,candle)
+
+print(unlock_bomb)
+
